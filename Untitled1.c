888f5061d77bfe3e141a1b5355e8730993ff52fb
Tarun-Dixit <92179532+Tarun-Dixit@users.noreply.github.com>
2025-08-04 22:45:43 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    float average = sum / n;
+    printf("Average = %.2f\n", average);
+
+    return 0;
+}
